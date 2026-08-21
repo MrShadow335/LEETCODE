8b0d7655f87a0b548a83969997b1fb9fa02ae9d5
Abhishek Kumar <letmeidentifymyself@gmail.com>
2026-08-21 20:34:42 +0530

    Time: 17 ms (16.63%), Space: 46.1 MB (50.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,40 @@
 class Solution {
     public boolean isAnagram(String str1, String str2) {
-        if (str1.length() != str2.length())
-            return false;
-        char[] arr1 = str1.toCharArray();
-        char[] arr2 = str2.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        for (int i = 0, j = 0; i < arr1.length; i++, j++) {
-            if (arr1[i] != arr2[j])
+        // if (str1.length() != str2.length())
+        //     return false;
+        // char[] arr1 = str1.toCharArray();
+        // char[] arr2 = str2.toCharArray();
+        // Arrays.sort(arr1);
+        // Arrays.sort(arr2);
+        // for (int i = 0, j = 0; i < arr1.length; i++, j++) {
+        //     if (arr1[i] != arr2[j])
+        //         return false;
+        // }
+        // return true;
+
+        if(str1.length() != str2.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<str1.length(); i++){
+            char ch = str1.charAt(i);
+            if(!map.containsKey(ch)){
+                map.put(ch,1);
+            }
+            else{
+                int freq = map.get(ch);
+                map.put(ch, freq+1);
+            }
+        }
+        for(int i=0; i< str2.length(); i++){
+            char ch = str2.charAt(i);
+            if(!map.containsKey(ch)){
                 return false;
+            }
+                int freq = map.get(ch);
+                if(freq == 0) return false;
+                map.put(ch, freq-1);
+            
         }
         return true;
-
         
 
     }
