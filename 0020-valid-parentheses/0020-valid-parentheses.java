39e4a744165fdd5e8fbb15a9f608cc6022d4b090
Abhishek Kumar <letmeidentifymyself@gmail.com>
2026-08-29 21:30:31 +0530

    Time: 4 ms (36.48%), Space: 43.3 MB (57.57%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if(n%2 != 0) return false;
+        Stack<Character> st = new Stack<>();
+        for(int i =0; i<n; i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch == '{' || ch == '[') st.push(ch);
+            else{
+                if(st.size() == 0) return false;
+                char top = st.peek();
+                if(sameStyle(top,ch)) st.pop();
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.size() == 0) return true;
+        return false;
+    }
+    public boolean sameStyle(Character a, Character b){
+        if(a =='(' && b==')') return true;
+        if(a == '{' && b == '}') return true;
+        if(a == '[' && b == ']') return true;
+        return false;
+    }
+}
